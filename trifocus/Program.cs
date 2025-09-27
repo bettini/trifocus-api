2fa15a364ea99db1440a607253f8da3c7a83b2bb
fbettini <florian.bettini@gmail.com>
2025-09-27 16:49:53 -0400

    Apply .gitignore rules and remove ignored files from tracking

--- trifocus/Program.cs
+++ trifocus/Program.cs
@@ -1,18 +1,37 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Trifocus API V1");
+        // Serve Swagger UI at application root
+        c.RoutePrefix = "swagger";
+    });
 }
+
+// Enable CORS
+app.UseCors();
 
 app.UseHttpsRedirection();
 
